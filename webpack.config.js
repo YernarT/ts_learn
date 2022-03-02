9ffc91e58c0c02c57e8dfead4449cf7b8c741d67
YernarT <toktaryernar@gmail.com>
2022-03-03 00:50:29 +0600

    update: :hammer: complete config, add less config

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,57 @@ module.exports = {
 			arrowFunction: false,
 		},
 	},
+	// Webpack 打包时要用的模块
+	module: {
+		// 加载规则
+		rules: [
+			{
+				// 规则生效范围
+				test: /\.ts$/,
+				// 指定 loader
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [
+								[
+									// 插件环境
+									'@babel/preset-env',
+									{
+										targets: {
+											'chrome': '58',
+											'ie': '11',
+										},
+										// corejs 版本
+										'corejs': '3',
+										// 使用 corejs 方式, usage表示 按需加载
+										'useBuiltIns': 'usage',
+									},
+								],
+							],
+						},
+					},
+					'ts-loader',
+				],
+				exclude: '/node-modules',
+			},
+
+			// 指定 less 文件处理方式
+			{
+				test: /\.less$/,
+				use: ['style-loader', 'css-loader', 'less-loader'],
+			},
+		],
+	},
+
+	// Webpack 插件
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HTMLWebpackPlugin({
+			template: 'src/index.html',
+		}),
+	],
+
+	// mode: 'development',
 	mode: 'production',
 };
