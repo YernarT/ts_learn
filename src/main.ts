044f394ee9157305472dfa6e0ea58d76f2be4779
YernarT <toktaryernar@gmail.com>
2022-03-03 19:25:56 +0600

    feat: :sparkles: 记分牌的类

--- src/main.ts
+++ src/main.ts
@@ -35,9 +35,46 @@ class Food {
 	}
 }
 
-// 测试代码
-const food = new Food();
+// 记分牌的类
+class ScroePanel {
+	// 记录分数和等级
+	score = 0;
+	level = 1;
 
-console.log(food.x, food.y);
-food.changePosition();
-console.log(food.x, food.y);
+	// 分数和等级所在的元素, 在构造函数中初始化
+	scoreDOM: HTMLElement;
+	levelDOM: HTMLElement;
+
+	// 限制等级上限
+	levelCap: number;
+	// 升级所需分数
+	scoreRequiredToLevelUp: number;
+
+	constructor(levelCap: number = 10, scoreRequiredToLevelUp: number = 10) {
+		this.scoreDOM = document.getElementById('score')!;
+		this.levelDOM = document.getElementById('level')!;
+
+		this.levelCap = levelCap;
+		this.scoreRequiredToLevelUp = scoreRequiredToLevelUp;
+	}
+
+	// 加分方法
+	addScore() {
+		this.score += 1;
+		this.scoreDOM.innerHTML = this.score.toString();
+
+		// 是否升级
+		if (this.score % this.scoreRequiredToLevelUp === 0) {
+			this.levelUp();
+		}
+	}
+
+	// 升级方法
+	levelUp() {
+		// 高于等级上限不改变
+		if (this.level < this.levelCap) {
+			this.level += 1;
+			this.levelDOM.innerHTML = this.level.toString();
+		}
+	}
+}
