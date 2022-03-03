95f3b31f4eedaca1b7dc52a65cb55cc8181d7fd9
YernarT <toktaryernar@gmail.com>
2022-03-03 19:49:44 +0600

    feat: :sparkles: add GameControl class

---
+++ src/modules/GameContorl.ts
@@ -0,0 +1,17 @@
+import Snake from './Snake';
+import Food from './Food';
+import ScorePanel from './ScorePanel';
+
+// 游戏控制器
+export default class GameControl {
+	snake: Snake;
+	food: Food;
+
+	scorePanel: ScorePanel;
+
+	constructor() {
+		this.snake = new Snake();
+		this.food = new Food();
+		this.scorePanel = new ScorePanel();
+	}
+}
