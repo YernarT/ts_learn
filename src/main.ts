2966e193980d83f924a069f93fd44ecdc23e3433
YernarT <toktaryernar@gmail.com>
2022-03-03 20:24:49 +0600

    update: :hammer: 使用游戏控制器

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style/reset.less';
 import './style/index.less';
 
-import Food from './modules/Food';
-import ScorePanel from './modules/ScorePanel';
+import GameControl from './modules/GameControl';
+
+new GameControl();
